043fe4331d28772070d7746896464f156252df31
ralmsdeveloper <ralms@ralms.net>
2017-12-17 10:11:11 -0200

    Clean Files

---
+++ _site/assets/js/lunr-en.js
@@ -0,0 +1,81 @@
+var idx = lunr(function () {
+  this.field('title', {boost: 10})
+  this.field('excerpt')
+  this.field('categories')
+  this.field('tags')
+  this.ref('id')
+});
+
+
+
+  
+  
+    idx.add({
+      title: "Novidade C# 7.3, o que vem por ai?",
+      excerpt: "Apesar da minha vontade ser compartilhar mais conteúdos relacionados às tecnologias EntityFramework Core e AspNet Core, sendo programador e amante...",
+      categories: ["CSharp"],
+      tags: ["CSharp"],
+      id: 0
+    });
+    
+  
+
+
+console.log( jQuery.type(idx) );
+
+var store = [
+  
+    
+    
+    
+      
+      {
+        "title": "Novidade C# 7.3, o que vem por ai?",
+        "url": "http://localhost:4000/csharp/csharp-novidade/",
+        "excerpt": "Apesar da minha vontade ser compartilhar mais conteúdos relacionados às tecnologias EntityFramework Core e AspNet Core, sendo programador e amante...",
+        "teaser":
+          
+            null
+          
+      }
+    
+  ]
+
+$(document).ready(function() {
+  $('input#search').on('keyup', function () {
+    var resultdiv = $('#results');
+    var query = $(this).val();
+    var result = idx.search(query);
+    resultdiv.empty();
+    resultdiv.prepend('<p class="results__found">'+result.length+' Result(s) found</p>');
+    for (var item in result) {
+      var ref = result[item].ref;
+      if(store[ref].teaser){
+        var searchitem =
+          '<div class="list__item">'+
+            '<article class="archive__item" itemscope itemtype="http://schema.org/CreativeWork">'+
+              '<h2 class="archive__item-title" itemprop="headline">'+
+                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
+              '</h2>'+
+              '<div class="archive__item-teaser">'+
+                '<img src="'+store[ref].teaser+'" alt="">'+
+              '</div>'+
+              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt+'</p>'+
+            '</article>'+
+          '</div>';
+      }
+      else{
+    	  var searchitem =
+          '<div class="list__item">'+
+            '<article class="archive__item" itemscope itemtype="http://schema.org/CreativeWork">'+
+              '<h2 class="archive__item-title" itemprop="headline">'+
+                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
+              '</h2>'+
+              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt+'</p>'+
+            '</article>'+
+          '</div>';
+      }
+      resultdiv.append(searchitem);
+    }
+  });
+});
